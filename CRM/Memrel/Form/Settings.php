7234489edb72256d0369c81238e170980a479d9a
Frank J. Gómez <frank@ginkgostreet.com>
2018-02-13 17:14:04 -0500

    Removed unneeded boilerplate.

--- CRM/Memrel/Form/Settings.php
+++ CRM/Memrel/Form/Settings.php
@@ -43,8 +43,6 @@ class CRM_Memrel_Form_Settings extends CRM_Core_Form {
       ),
     ));
 
-    // export form elements
-    $this->assign('elementNames', $this->getRenderableElementNames());
     parent::buildQuickForm();
   }
 
@@ -77,27 +75,6 @@ class CRM_Memrel_Form_Settings extends CRM_Core_Form {
     }
 
     return $this->shadowRelationshipTypeId;
-  }
-
-  /**
-   * Get the fields/elements defined in this form.
-   *
-   * @return array (string)
-   */
-  public function getRenderableElementNames() {
-    // The _elements list includes some items which should not be
-    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
-    // items don't have labels.  We'll identify renderable by filtering on
-    // the 'label'.
-    $elementNames = array();
-    foreach ($this->_elements as $element) {
-      /** @var HTML_QuickForm_Element $element */
-      $label = $element->getLabel();
-      if (!empty($label)) {
-        $elementNames[] = $element->getName();
-      }
-    }
-    return $elementNames;
   }
 
 }
